df76ec6e1d54af3c3e47266338b5069fd7443544
Kushal Neupane <mkushal10@gmail.com>
2020-08-29 12:13:19 +0545

    I commit

--- J/program.py
+++ J/program.py
@@ -3,5 +3,5 @@ for row in range(7):
         if col==2 or ((row==0 and col!=2) and col!=2) or (row==6 and col<2):
             print("*",end="")
         else:
-            print(end=" ")
+            print(end=" ")a
     print()
